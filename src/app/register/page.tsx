593e00eace847d82fb1e5aa9b193b2038053b931
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-05-13 20:35:48 +0600

    completed regestir page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,27 +1,32 @@
 import Link from "next/link";
 import styles from "../../shared/form.module.css";
 
-const Login = () => {
+const Register = () => {
   return (
     <main className={styles.main}>
       <div className={styles.formContainer}>
-        <h3>Wellcome Back!</h3>
+        <h3>Please Register!</h3>
         <form className={styles.form}>
+          <label>
+            <input type="text" placeholder="Full Name" />
+          </label>
           <label>
             <input type="text" placeholder="Email" />
           </label>
           <label>
             <input type="text" placeholder="Password" />
           </label>
-          <input type="submit" className="primary-btn" value="Login" />
+          <label>
+            <input type="text" placeholder="Confirm Password" />
+          </label>
+          <input type="submit" className="primary-btn" value="Register" />
         </form>
         <p>
-          Don&apos;t have any account? Please{" "}
-          <Link href="/register">Register</Link>
+          Already have an account? Please <Link href="/login">Login</Link>
         </p>
       </div>
     </main>
   );
 };
 
-export default Login;
+export default Register;
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -4,6 +4,11 @@ const Footer = () => {
   return (
     <footer className={styles.footer}>
       <span>2024 &copy; All copyrights reserve of HotelBA</span>
+      <ul>
+        <li>Privacy Policy</li>
+        <li>Terms and Conditions</li>
+        <li>Career</li>
+      </ul>
     </footer>
   );
 };
